1b772e2855a6134009fdae74865ceb7a7e7a8f48
Julian Sarmiento <juliansarmiento10@outlook.com>
2021-11-21 20:03:44 -0500

    ready CR falta UD no funciona metodo PUT

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,40 @@
-const router = require ('express').Router();
+const router = require("express").Router();
 
-router.get('/notes', (req, res)=>{
-    res.send('notas aquí');
+const Notes = require("../models/Notes");
+
+router.get("/notes/add", (req, res) => {
+  res.render("notes/new_note");
+});
+router.post("/notes/new_note", async(req, res) => {
+  const { title, description } = req.body;
+  const error = [];
+  if (!title || !description) {
+    error.push({ text: "Please Write title or description" });
+  }
+  if (error.length > 0) {
+    res.render("notes/new_note", {
+      error,
+      title,
+      description,
+    });
+  } else {
+    const newNote  = new Notes({title, description});
+    await newNote.save();
+    res.redirect("/notes");
+  }
+});
+router.get("/notes", async(req, res) => {
+  const notes = await Notes.find().sort({date: 'desc'});
+  res.render('notes/all_notes', {notes})
+});
+router.get("/notes/edit/:_id", async(req, res)=>{
+    const note = Notes.findById(req.params._id);
+    res.render('notes/edit_note', {note});
+});
+
+router.put('/notes/edit_note/:_id', async(req, res)=>{
+    const {title, description} =req.body;
+    await Notes.findByIdAndUpdate(req.params._id, {title, description})
+    res.redirect('/notes');
 });
 module.exports = router;
