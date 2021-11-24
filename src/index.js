6e7eddd56487c5096911258c490b1434321e04ad
Julian Sarmiento <juliansarmiento10@outlook.com>
2021-11-24 17:37:43 -0500

    autenticacion a medias

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const exphbs  =  require('express-handlebars');
 const methodOverride = require ('method-override');
 const session = require ('express-session');
 const flash = require('connect-flash');
+const passport = require('passport')
 //initilizators
 const app = express();
 require('dotenv').config({path: 'src/.env'});
 require('./database');
+require('./config/passport');
 //settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -32,11 +34,14 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash());
 //Global variables
 app.use((req, res, next)=>{
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next();
 });
 //routes
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,19 +1,36 @@
 const router = require("express").Router();
+const Users = require("../models/Users");
+const user = require("../models/Users")
+const passport = require('passport');
 router.get("/users/signin", (req, res) => {
   res.render("users/signin");
 });
+router.post('/users/signin', passport.authenticate('local', {
+  successRedirect: '/notes',
+  failureRedirect: '/users/signin',
+  failureFlash: true
+}));
 router.get("/users/signup", (req, res) => {
   res.render("users/signup");
 });
-router.post("/users/signup", (req, res) => {
-  const {nombre, email, password, confirmPassword} = req.body;
+router.post("/users/signup", async(req, res) => {
+  const {name, email, password, confirmPassword} = req.body;
   const error= [];
-  if(password != confirmPassword || password.length < 4 || nombre.length <=0 ){
+  if(password != confirmPassword || password.length < 4 || name.length<= 0 ){
     error.push({text: 'La contraseña no coincide o contiene menos de 4 digitos o un campo esta vacio'})
   }if(error.length > 0){
-    res.render('users/signup', { error, nombre, email, password, confirmPassword  })
+    res.render('users/signup', { error, name, email, password, confirmPassword  })
   }else{
-      res.send("ok");
+    const emailUser = await Users.findOne({email: email});
+    if(emailUser){
+      req.flash('error_msg', 'El email ya esta en uso');
+      res.redirect('/users/signup');
+    }
+    const newUser = new Users({name, email, password});
+    newUser.password = await newUser.encryptPassword(password);
+    await newUser.save();
+    req.flash('success_msg', 'Se registro correctamente');
+    res.redirect('/users/signin');
   }
 });
 module.exports = router;
